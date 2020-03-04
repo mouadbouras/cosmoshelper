ad3282f9c7b7001dab5ab12443a5c6a9f7ad3df5
Mouad Bouras <mouad.bouras@randstad.ca>
2020-03-04 15:28:55 -0500

    test

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ exec("netstat -ano | findstr \"pid: 8081\"", (error, stdout, stderr) => {
         console.log(`stderr: ${stderr}`);
         return;
     }
-    console.log(`stdout: ${stdout}`);
+    console.log(`stdout: ${stdout.trim()}`);
     console.log("\n\n\n");
     const pid = getPortFromCommand(stdout);
     console.log(getPortFromCommand(`process found: ${pid}`));
 });
 
 function getPortFromCommand(output) { 
+    console.log("Length is : " + output.length);
     return output.trim().substr(output.length - 5);
 }
